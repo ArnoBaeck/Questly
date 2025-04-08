356df46cc28ed40548be8c23ff28ae307bb06b99
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2025-04-08 10:58:11 +0200

    Initial Commit

---
+++ Questly/QuestlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestlyApp.swift
+//  Questly
+//
+//  Created by Arno Baeck on 08/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuestlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
